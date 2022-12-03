2ad7ff70611bfb8a7bc65bccecf089b66915d27b
Julia Hardikova <iylia.hardikova@mail.ru>
2022-12-03 18:07:10 +0300

    Version JaCoCo

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class RadioTest {
 
     @Test
-    public void switchingRadioStations() { 
+    public void switchingRadioStations() {
         Radio cond = new Radio();
         cond.setRadioStationNumber(7);
 
